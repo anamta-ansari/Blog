3dda70455a5c37f5195e69d1467b0685b292f0a9
anamta-ansari <161108965+anamta-ansari@users.noreply.github.com>
2024-12-31 21:37:38 +0500

    Update About.tsx

--- src/app/Components/About.tsx
+++ src/app/Components/About.tsx
@@ -1,20 +1,22 @@
-export default function About(){
-    return(
+export default function About() {
+    return (
         <>
-        <div className="w-full font-sans">
-        <div className="mx-auto px-[2rem] py-[5rem] flex flex-col items-center justify-center h-max 2xl:w-[1500px]">
-        <h1 className="text-[48px] font-bold mb-[0.5rem] ">About Us</h1>
-        <h1 className="text-[42px] mb-[0.5rem] font-bold ">Exploring the Future of Innovation and Technology</h1>
-        <p className="text-[18px] mb-[0.5rem] ">Welcome to our platform, where we keep you at the forefront of the ever-evolving world of technology. Stay updated with the latest breakthroughs, trends, and insights that are shaping tomorrow's digital landscape. From AI and blockchain to gadgets and software, we cover the stories that matter most in the tech world.
-        <span className = "block my-5">
-        Our team of experts is passionate about exploring the cutting-edge innovations revolutionizing industries and transforming the way we live and work. We delve into emerging technologies like quantum computing, IoT, and more, providing you with a deep understanding of their potential impact on the future.
-        </span>
-        <span className = "block my-5">
-        In addition to the latest news, we offer in-depth analysis, exclusive interviews with thought leaders, and real-world case studies to ensure you're always ahead of the curve in the fast-paced tech industry. Whether you're a tech enthusiast, professional, or simply curious about the future, our content is designed to inform and inspire.
-        </span>
-        Join us on this exciting journey as we navigate the digital frontier and uncover the technologies that will define the next generation. Stay informed, stay ahead, and be part of the future with us.</p>
-        </div>
-        </div>
+            <div className="w-full font-sans">
+                <div className="mx-auto px-[2rem] py-[5rem] flex flex-col items-center justify-center h-max 2xl:w-[1500px]">
+                    <h1 className="text-[48px] font-bold mb-[0.5rem]">About Us</h1>
+                    <h1 className="text-[42px] mb-[0.5rem] font-bold">Exploring the Future of Innovation and Technology</h1>
+                    <p className="text-[18px] mb-[0.5rem]">
+                        Welcome to our platform, where we keep you at the forefront of the ever-evolving world of technology. Stay updated with the latest breakthroughs, trends, and insights that are shaping tomorrow's digital landscape. From AI and blockchain to gadgets and software, we cover the stories that matter most in the tech world.
+                        <span className="block my-5">
+                            Our team of experts is passionate about exploring the cutting-edge innovations revolutionizing industries and transforming the way we live and work. We delve into emerging technologies like quantum computing, IoT, and more, providing you with a deep understanding of their potential impact on the future.
+                        </span>
+                        <span className="block my-5">
+                            In addition to the latest news, we offer in-depth analysis, exclusive interviews with thought leaders, and real-world case studies to ensure you're always ahead of the curve in the fast-paced tech industry. Whether you're a tech enthusiast, professional, or simply curious about the future, our content is designed to inform and inspire.
+                        </span>
+                        Join us on this exciting journey as we navigate the digital frontier and uncover the technologies that will define the next generation. Stay informed, stay ahead, and be part of the future with us.
+                    </p>
+                </div>
+            </div>
         </>
-    )
+    );
 }
